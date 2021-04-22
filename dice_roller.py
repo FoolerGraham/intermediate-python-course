7063b8d99b4af7c4bd02c271baa8f318fddbc4f7
FoolerGraham <hayhayking@outlook.com>
2021-04-22 00:17:53 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,16 +5,17 @@ def main():
   dice_rolls = 2
   dice_sum = 0
 
-  for i in range(0, dice_rolls):
-    roll = random.randint(1, 6)
-    dice_sum += roll
-    if roll == 1:
-      print(f'you rolled a {roll}, critical fail!')
-    elif roll == 6:
-      print(f'you rolled a {roll}, critical success!')
-    else:
-      print(f'You rolled {roll}')
-  print(f'you have rolled a total of {dice_sum}')
+  if input("please enter your name: "):
+    for i in range(0, dice_rolls):
+      roll = random.randint(1, 6)
+      dice_sum += roll
+      if roll == 1:
+        print(f'you rolled a {roll}, critical fail!')
+      elif roll == 6:
+        print(f'you rolled a {roll}, critical success!')
+      else:
+        print(f'You rolled {roll}')
+    print(f'you have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
